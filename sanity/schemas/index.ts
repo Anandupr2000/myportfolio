8f75b95b6837de9574895d43fe65e90d1d37ca67
Anandu <ananduprajesh@gmail.com>
2023-02-21 20:34:16 +0530

    fixing sanity error of module not found by editing import statement

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -4,7 +4,7 @@ import education from './education'
 import projects from './projects'
 import skill from './skill'
 import social from './social'
-import { createSchema } from 'sanity'
+// import { createSchema } from 'sanity'
 
 export const schemaTypes = [basicInfo, experience,education, projects, skill, social]
 
