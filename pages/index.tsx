3b16217df34cee20bbadd6029d7cf8cf43c472ad
Anandu <ananduprajesh@gmail.com>
2023-06-08 23:14:53 +0530

    added div to display project links

--- pages/index.tsx
+++ pages/index.tsx
@@ -104,8 +104,8 @@ export default function Home({ basicInfo, experiences, skills, projects, socials
 //   return { paths, fallback: 'blocking' };
 // }
 
-// export const getStaticProps: GetStaticProps<Props> = async () => {
-  export const getServerSideProps: GetStaticProps<Props> = async () => {
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  // export const getServerSideProps: GetStaticProps<Props> = async () => {
 
   // all data is fetched from baseurl which is "https://anandu-portfolio-5v92am8tq-21mp1820-ritacin.vercel.app/" @ vercel
   const basicInfo: BasicInfo = await fetchBasicInfo();
